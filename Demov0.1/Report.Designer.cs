00c132ef8fd4b420bfe13f90ad42cf346906d821
Chokprapat <leemaga8@gmail.com>
2025-02-09 15:06:06 +0700

    เเก้ไข

--- Demov0.1/Report.Designer.cs
+++ Demov0.1/Report.Designer.cs
@@ -112,7 +112,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 37);
             this.button1.TabIndex = 7;
-            this.button1.Text = "กดเลย";
+            this.button1.Text = "พิมพ์";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
